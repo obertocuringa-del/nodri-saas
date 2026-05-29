cffd7c84fc28872063abb1b9547b11df83442cc6
obertocuringa-del <obertocuringa@gmail.com>
2026-05-29 07:02:00 -0300

    Create page.tsx

---
+++ src/app/logout/page.tsx
@@ -0,0 +1,8 @@
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+export default function LogoutPage() {
+  const cookieStore = cookies()
+  cookieStore.delete('nodri_token')
+  redirect('/login')
+}
